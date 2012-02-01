58451c738b9e203f882e30ba17aaf3ffa5ab10e2
grosser <grosser.michael@gmail.com>
2012-01-31 20:08:52 -0800

    refactor a bit

--- lib/parallel_split_test/command_line.rb
+++ lib/parallel_split_test/command_line.rb
@@ -7,27 +7,34 @@ module ParallelSplitTest
       setup_copied_from_rspec(err, out)
 
       processes = Parallel.processor_count
-      example_count = @world.example_count / processes
-      seed = (@configuration.randomize? ? @configuration.seed : nil)
 
       Parallel.in_processes(processes) do |process_number|
         ENV['TEST_ENV_NUMBER'] = (process_number == 0 ? '' : (process_number + 1).to_s)
-
-        @configuration.reporter.report(example_count, seed) do |reporter|
-          begin
-            @configuration.run_hook(:before, :suite)
-            groups = groups_for_this_process(@world.example_groups.ordered, process_number, processes)
-            puts "PROCESS #{process_number} -- #{groups.size}"
-
-            groups.map {|g| g.run(reporter)}.all? ? 0 : @configuration.failure_exit_code
-          ensure
-            @configuration.run_hook(:after, :suite)
-          end
-        end
+        run_group_of_tests(processes, process_number)
       end
     end
 
     private
+
+    def run_group_of_tests(processes, process_number)
+      example_count = @world.example_count / processes
+
+      @configuration.reporter.report(example_count, seed) do |reporter|
+        begin
+          @configuration.run_hook(:before, :suite)
+          groups = groups_for_this_process(@world.example_groups.ordered, process_number, processes)
+          puts "PROCESS #{process_number} -- #{groups.size}"
+
+          groups.map {|g| g.run(reporter)}.all? ? 0 : @configuration.failure_exit_code
+        ensure
+          @configuration.run_hook(:after, :suite)
+        end
+      end
+    end
+
+    def seed
+      @configuration.randomize? ? @configuration.seed : nil
+    end
 
     def groups_for_this_process(groups, number, count)
       selected = []
